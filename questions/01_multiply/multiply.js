69e1cd70e7727d2fd02c9ca727387b95458b3c1b
Cynthia E <cynthia8499@hotmail.com>
2020-04-25 09:32:13 -0400

    solution stretch question

--- questions/01_multiply/multiply.js
+++ questions/01_multiply/multiply.js
@@ -4,7 +4,9 @@ const multiply = (num1, num2) => {
     if(typeof num1 !== 'number' || typeof num2 !== 'number'){
     throw Error
     }
+    return num1*num2
 
 };
 
+console.log(multiply ('@','_'))
 module.exports = { multiply };
--- questions/01_multiply/multiply.test.js
+++ questions/01_multiply/multiply.test.js
@@ -4,20 +4,20 @@ describe('multiply testing', () => {
   test('it should ensure it receives two numbers', () => {
     // test some values that should fail
 
-    expect(() => multiply(/*fill this in*/)).toThrow();
+    expect(() => multiply('a', 'b')).toThrow(Error);
 
-    expect(() => multiply(/*fill this in*/)).toThrow();
+    expect(() => multiply('_', "@")).toThrow(Error);
   });
 
   test('it returns the product of two numbers', () => {
     // test the type of the returned value
-    expect(/*fill this in*/).toBe(/*fill this in*/);
+    expect(multiply(85,19)).toBe(1615);
 
     // test that the returned value is correct
-    expect(/*fill this in*/).toBe(/*fill this in*/);
+    expect(multiply(9,7)).toBe(63);
 
     // test some other values
-    expect(/* fill this in */).toBe(/* fill this in */);
+    expect(multiply(10,9)).toBe(90);
   });
 
   test('it is not hardcoded (hint: use random numbers)', () => {
