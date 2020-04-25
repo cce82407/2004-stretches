9f9df540e3fc6a116d4550fb88f75bd914a0afc8
Cynthia E <cynthia8499@hotmail.com>
2020-04-25 09:19:06 -0400

    stretch solution multiply

--- questions/01_multiply/multiply.js
+++ questions/01_multiply/multiply.js
@@ -1,5 +1,10 @@
 // This stretch requires you to write your own test cases
 
-const multiply = (num1, num2) => {};
+const multiply = (num1, num2) => {
+    if(typeof num1 !== 'number' || typeof num2 !== 'number'){
+    throw Error
+    }
+
+};
 
 module.exports = { multiply };
