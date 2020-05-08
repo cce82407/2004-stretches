b9f3a7d283166d4df75473dd7db6093bcc77f26b
Cynthia E <cynthia8499@hotmail.com>
2020-05-07 20:14:03 -0400

    trim solution

--- questions/08_trim/trim.js
+++ questions/08_trim/trim.js
@@ -1,6 +1,15 @@
 // trim takes an object and removes any keys that have undefined or null values
 
 const trim = obj => {
+  let finalObj = obj
+  for(let key in obj){
+    if (obj[key] === undefined || obj[key] === null){
+      delete obj[key]
+    } 
+    else finalObj[key] = obj[key]
+  }
+  console.log(finalObj)
+  return finalObj 
   // YOUR CODE
 };
 
