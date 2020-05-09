56d29bbca55a8032fa62c9678e6d92538171e13c
Cynthia E <cynthia8499@hotmail.com>
2020-05-09 09:12:05 -0400

    solution fail

--- questions/10_splatter/splatter.js
+++ questions/10_splatter/splatter.js
@@ -1,7 +1,10 @@
 //write the splatter function
 
-const splatter = () => {
-  //write code here
+const splatter = (...args) => {
+  const finalVal = 0
+  console.log(s[...args])
+  argsArr.forEach(obj => finalVal+= Object.values(obj)[0])
+  return finalVal//write code here
 };
 
 module.exports = { splatter };
