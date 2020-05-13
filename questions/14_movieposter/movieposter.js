1e1dcd1bd0cd7eb6102144e72453ba35ca92e313
Cynthia E <cynthia8499@hotmail.com>
2020-05-13 18:51:09 -0400

    movieposter soln

--- questions/14_movieposter/movieposter.js
+++ questions/14_movieposter/movieposter.js
@@ -7,7 +7,24 @@
 // * Play *
 // ********
 
-const movieposter = () => {
+const movieposter = (words) => {
+  if (words.length === 0){
+    throw Error
+  }
+  moviePoster = ''
+  let maxLength = 0
+  for (let i = 0; i <words.length; i++){
+    if (words[i].length > maxLength){
+      maxLength = words[i].length
+    }
+  }
+  console.log(maxLength)
+  for (let j = 0; j < words.length + 1; j++){
+    if (i === 1){
+      moviePoster.push("*")[0])
+    }
+  }
+  console.log(moviePoster)
   //write code here
 };
 
