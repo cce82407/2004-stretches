4c06b7b2d3353d524b96f58d5fbdf1f11cdd1a06
Cynthia E <cynthia8499@hotmail.com>
2020-05-16 18:13:42 -0400

    floydsTriangle potential solution

--- questions/16_floydsTriangle/floydsTriangle.js
+++ questions/16_floydsTriangle/floydsTriangle.js
@@ -9,12 +9,36 @@
 
    
 const floydsTriangle = n => {
-    if (n === 1){
+    if (n === 0){
+        return ''
+    }
+    
+    const triangleNum = (n) => {
+        if (n===0){
+            return 0
+        }
+        if (n === 1){
         return 1
     }
-    return floydsTriangle(n-1)+n 
+    return triangleNum(n-1)+n 
+    
+    }
+    let finalTriangle = ''
+    
+    for (let i = 1; i <= n; i++ ){
+        let row = ''
+        for (let j = triangleNum(i-1)+1; j <=  triangleNum(i); j++){
+            row += j
+        }
+        finalTriangle += `${row}` + '\n'
+    }
+    if (finalTriangle.length === 1){
+        return '1'
+    }
+    return finalTriangle
 }
+console.log(floydsTriangle(1))
 
-console.log(floydsTriangle(5))
+
 
 module.exports = { floydsTriangle };
