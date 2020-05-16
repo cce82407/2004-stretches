03ffcc94f7a8b1c99c9cbd94cc2f59bf1bc9a812
Cynthia E <cynthia8499@hotmail.com>
2020-05-16 09:21:06 -0400

    bookReview soln

--- questions/17_bookReview/bookReview.js
+++ questions/17_bookReview/bookReview.js
@@ -1,2 +1,26 @@
-class BookReview {}
+class BookReview {
+    constructor(title, author){
+    this.title = title
+    this.author = author
+    this.notes = new Array (15)
+    this.rating = 0
+    }
+
+    addNotes (page, note){
+        if(this.notes.length === 0){
+        this.notes = new Array(page)
+        }
+        if (this.notes[page]){
+        this.notes.push(note)
+        }
+        else (this.note[page] = note)
+
+    }
+    addRating (num){
+        if (num < 0 || num > 5){
+            throw Error
+        }
+        else this.rating = num
+    }
+}
 module.exports = { BookReview };
