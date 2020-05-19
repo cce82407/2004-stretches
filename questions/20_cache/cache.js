9ac9b2cd68e978d15a4a3d8f5e3e6b2aee187e5a
Cynthia E <cynthia8499@hotmail.com>
2020-05-19 19:45:24 -0400

    cache solution

--- questions/20_cache/cache.js
+++ questions/20_cache/cache.js
@@ -1,7 +1,16 @@
 // see test specs
 
-function cache() {
-  // YOUR CODE
+function cache(func) {
+  if (typeof func !== 'function'){
+    throw Error ('Input must be a function.')
+  }
+  let prevArg = ''
+  return function(arg){
+    if (prevArg !== arg.toString()){
+      prevArg += arg.toString()
+     return func(arg)
+  }
+}
 }
 
 module.exports = { cache };
