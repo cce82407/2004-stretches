7f51b2ef988e33d513fe53c435750f0d5ba0ef4c
Cynthia E <cynthia8499@hotmail.com>
2020-05-23 00:54:51 -0400

    extension solution

--- questions/23_extension/extension.js
+++ questions/23_extension/extension.js
@@ -1,5 +1,29 @@
 //write a function that takes a function, and makes it callable with two invocations
 
-const extensions = () => {};
+const extensions = (func) => {
+    // if (typeof num1 !== 'number'|| typeof num2 !== 'number'){
+    //     throw Error
+    // }
+    //counter = 0
+    //let value
+    if (typeof func !== 'function'){
+        throw Error
+    }
+     return function(num1){
+        if (typeof num1 !== 'number'){
+            throw Error
+        }
+         return function(num2){
+             if (typeof num2 !== 'number'){
+                 throw Error
+             }
+             return func(num1, num2)
+         }
+     }
+
+     
+    
+};
+
 
 module.exports = { extensions };
