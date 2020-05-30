594d79ae85f45a0ea879ae747f8d1b555be4a014
Cynthia E <cynthia8499@hotmail.com>
2020-05-30 09:20:31 -0400

    almost solution!

--- questions/29_btownbakery/btownbakery.js
+++ questions/29_btownbakery/btownbakery.js
@@ -1,7 +1,15 @@
 // B-Town bakery over-bought the supplies to make cake and needs to know how many they can make all together.
 
-const btownbake = () => {
-  //enter code here
+const btownbake = (recipe, supplies) => {
+  if (typeof recipe !== 'object' || typeof supplies !== 'object'){
+    throw Error
+  }
+  let possibleCakes = {}
+  for (let ing in recipe){
+    possibleCakes[ing] = Math.floor(supplies[ing]/recipe[ing])
+    
+  }
+  return Math.min(...Object.values(possibleCakes))
 };
 
 module.exports = { btownbake };
