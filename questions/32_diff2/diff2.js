66506a6cd43031b1018de68aea48082d59a9aa28
Cynthia E <cynthia8499@hotmail.com>
2020-06-02 18:51:49 -0400

    3/4 specs aint bad!

--- questions/32_diff2/diff2.js
+++ questions/32_diff2/diff2.js
@@ -2,6 +2,23 @@
 // the returning array of pairs should be sorted in ascending order of values.
 //diffTwo([1, 2, 3, 4])=>[[1, 3], [2, 4]]
 
-const diffTwo = () => {};
+const diffTwo = (arr) => {
+    if(!Array.isArray(arr)){
+        throw Error
+    }
+    let diffTwoArr = []
+    for (let i = 0; i < arr.length-1; i++){
+        for(let j = i + 1; j < arr.length; j++){
+            if (Math.abs(arr[i]-arr[j]) === 2){
+                let sortedEl = [arr[i], arr[j]].sort(function (a,b){
+                    return a-b
+                })
+                diffTwoArr.push(sortedEl)
+            }
+            console.log(diffTwoArr)
+        }
+    }
+    return diffTwoArr.sort()
+};
 
 module.exports = { diffTwo };
