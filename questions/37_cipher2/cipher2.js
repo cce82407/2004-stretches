8a9c3272d4bc6bbdefe4884bdc0f65c12f99a69b
Cynthia E <cynthia8499@hotmail.com>
2020-06-08 18:55:01 -0400

    cipher 2 fail

--- questions/37_cipher2/cipher2.js
+++ questions/37_cipher2/cipher2.js
@@ -1,4 +1,12 @@
-const decypher = () => {
+const decypher = (str) => {
+  let strArr = str.split('')
+  const num = [0,1,2,3,4,5,6,7,8,9]
+  if (num.inludes(strArr[0])){
+    console.log(true)
+    return true
+  }
+  console.log(strArr)
+  //for (let )
   //YOUR CODE HERE
 };
 module.exports = { decypher };
