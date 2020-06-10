465701742b704e7ede6fceb542c3d40519eb433e
Cynthia E <cynthia8499@hotmail.com>
2020-06-10 18:54:57 -0400

    definite getstudents fail

--- questions/38_sortOrder/sortOrder.js
+++ questions/38_sortOrder/sortOrder.js
@@ -6,10 +6,7 @@ and a key for the return value.
 function sortOrder(data) {
   const sortOrdersSorted = data.sort(function (a,b) {
     return a.sortOrder - b.sortOrder}).map(e => e.name)
-
   return sortOrdersSorted
-
-  // YOUR CODE
 }
 
 module.exports = { sortOrder };
--- questions/39_getstudents/getstudents.js
+++ questions/39_getstudents/getstudents.js
@@ -4,8 +4,12 @@ const axios = require('./axios');
 //returning that specific student as specified in the test
 //* you don't need to NPM i axios for this test... Only work within this file and use promises
 
-const getStudent = () => {
-  //code in here
+const getStudent = (name) => {
+  if (typeof name !== 'string'){
+    throw new Error
+  }
+  const data = axios.get('https://fullstack.com/api/cohort2004').data
+  return  data
 };
 
 module.exports = { getStudent };
