21295e64df0b0c0efa33e1a2a27aa1ceab17f0be
Cynthia E <cynthia8499@hotmail.com>
2020-06-11 21:12:44 -0400

    boxes success

--- questions/40_boxes/boxes.js
+++ questions/40_boxes/boxes.js
@@ -1,3 +1,17 @@
 //YOUR CODE HERE
 
+
+class Box {
+    constructor(color, capacity, contents){
+        this.color = color,
+        this.capacity = capacity,
+        this.contents = []
+    }
+    
+    pack(color, capacity){
+        const newBox = new Box(color, capacity)
+        this.contents.push(newBox)
+    }
+
+}
 module.exports = { Box };
